ac0ab1929796469eeaa797a6be31f7a1c66fac43
jairo caro <Jairo>
2020-12-02 02:16:35 -0500

    hola

--- pro.py
+++ pro.py
@@ -45,7 +45,7 @@ print(total1)
 print(total2)
 print(type(total1))
 obj=np.array([])
-
+j=0
 try:
         cnx = mysql.connector.connect(user='Jairo', password='Jairo1012#',
                                       host='127.0.0.1',
